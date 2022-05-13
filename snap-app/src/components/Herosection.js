82569f6561f4d613c92322c7f7731f44f43035ec
reda hammada <hammada.reda.dev@gmail.com>
2022-05-13 09:50:35 +0100

    herosection being wokred on

---
+++ snap-app/src/components/Herosection.js
@@ -0,0 +1,23 @@
+import React from "react";
+import hero from "./images/image-hero-desktop.png";
+
+const Herosection = () => {
+
+     return(
+
+        <div>
+            <div>
+               
+
+            </div>
+           
+            <div>
+                    <img src ={hero}   alt="hero section" />
+            </div>
+        </div>
+
+     )
+
+}
+
+export default Herosection;
---
+++ snap-app/src/components/Make.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Make = (props) => {
+
+    return(
+        
+        <div>
+            <h1>props.heading</h1>
+        </div>
+        <div>
+            
+        </div>
+    )
+}
